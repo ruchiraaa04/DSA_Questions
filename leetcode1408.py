bd990541a7c38ca3bbed672d17616733ff510a61
ruchiraaa04 <ruchiramore04@gmail.com>
2025-01-07 19:07:30 +0530

    String Matching

---
+++ leetcode1408.py
@@ -0,0 +1,11 @@
+def stringMatching(words):
+    result = []
+    for w1 in words:
+        for w2 in words:
+            if w1 != w2 and w1 in w2:
+                result.append(w1)
+                break  # No need to check further if `w1` is already a substring
+    return result
+    
+words=["mass","as","hero","superhero"]
+print(stringMatching(words))
